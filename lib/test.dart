5e8550c390e6cacab3823bd0cccf0f65f91a692a
Chatchon Damrongekarun <monalichic@gmail.com>
2021-12-12 12:42:00 +0700

    Initial commit

---
+++ lib/test.dart
@@ -0,0 +1,48 @@
+//top level function
+import 'dart:io';
+import 'dart:math';
+
+void main(){
+  /*print('Hello');
+  dynamic  i = 1;
+  print(i);
+  i="Hello";
+  print(i);*/
+
+  //ภาษา dart จะ inter type ให้เรา ดูจากต่าเริ่มต้นที่เรากำหนด
+  //กรบวนการ type inference
+  //var i=123; //int
+
+
+  var i = 0;
+  
+  
+  
+  /*var r =  Random();
+  var answer = r.nextInt(5);
+  print(answer);*/
+
+
+  var r = Random();
+  var answer = r.nextInt(5);
+  print(answer);
+
+
+  while(i<5) {
+    stdout.write("enter your name :");
+    var input = stdin.readLineSync();
+    var guess=int.tryParse(input!);
+    if(guess==answer){
+      print("correct");
+      break;
+    }
+    else{
+      print("false");
+    }
+    i++;
+  }
+
+}
+
+
+
