19d9e50f11181d08919801e04b90ed316086d4b5
Chatchon Damrongekarun <monalichic@gmail.com>
2022-01-14 15:57:12 +0700

    commot

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -27,66 +28,56 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('GUESS THE NUMBER')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Container(
-            decoration: BoxDecoration(color: Colors.greenAccent),
-            child:Center(
-                child:Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset('assets/images/guess_logo.png',width:100,height:100,),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text('GUESS',style:TextStyle(fontSize: 40,color: Colors.lightBlue),),
-                            Text('THE NUMBER',style:TextStyle(fontSize: 20,color: Colors.lightBlue),),
-                          ],)
-                      ],
-                    ),
-                    SizedBox(height: 40,),
-                    Padding(
-                      padding: const EdgeInsets.all(40.0),
-                      child: TextField(),
-                    ),
-                    ElevatedButton(onPressed: (){},child: Text('GUESS'),),
-                  ],
-                )
-            )
-        ),
+      appBar: AppBar(
+        title: Text('GUESS THE NUMBER'),
       ),
-    );
-/*      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          decoration: BoxDecoration(
-              color: Colors.lightBlueAccent,
-              border: Border.all(width: 2, color: Colors.black),
-              boxShadow: const [BoxShadow(
-              )]),
-          alignment: Alignment.center,
-          child: Center(
-            child: Column(
-              //mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/guess_logo.png'),
-                TextField(),
-                ElevatedButton(
-                  child: Text('Random'),
-                  onPressed: (){
-                  },
-                )
-              ],
-            ),
+      body: Container(// เป็นกล่องๆหนึ่งที่ไว้เอาของยัดเข้ามา
+        decoration: BoxDecoration(
+            //สำหรับตกแต่ง
+            color: Colors.lightBlueAccent /*.withOpacity(0.5)   สีโปร่งใส*/,
+            border: Border.all(width: 2.0, color: Colors.green),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  offset: Offset(2.0, 5.0), // (x ถ้าพวกไปขวา,y ถ้าบวกลงล่าง)
+                  blurRadius: 5.0, //สีเบลอ
+                  spreadRadius: 5.0 // ไกลออกไป
+                  )
+            ]),
+        //alignment: Alignment.center,
+        child: Center(// alt+enter  wrap center
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/guess_logo.png',width:100,height:100,),
+                  Column(
+                    children: [
+                      Text('GUESS',style: TextStyle(fontSize: 50.0 ,color: Colors.cyanAccent),),
+                      Text('THE NUMBER',style: TextStyle(fontSize: 30.0 ,color: Colors.cyanAccent),),
+                    ],
+                  ),
+
+
+                ],
+              ),
+              SizedBox(height: 20,),
+              Padding(
+                padding: const EdgeInsets.all(40.0),
+                child: TextField(),
+              ),
+
+              ElevatedButton(
+                child: Text('GUESS'),
+                onPressed: () {},
+              ),
+            ],
           ),
         ),
       ),
-    );*/
+    ); //เปลี่ยนพื้นหลังเป็นสีขาว
   }
 }
